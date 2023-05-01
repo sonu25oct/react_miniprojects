cc5d56643b4a5abf421a6c1c5f6147e26ad5020f
sonujha2022 <sonujha2022@users.noreply.github.com>
2023-05-01 17:35:39 +0530

    change color

--- src/01_randomizeColor/RandomizeColor.jsx
+++ src/01_randomizeColor/RandomizeColor.jsx
@@ -3,11 +3,25 @@ import Title from "../Title";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const RandomizeColors = () => {
-  const colorHandler = () => {
-    console.log("clicked");
+  const colorHandler = (e) => {
+    // console.log(ColorHeandler());
+    let body = document.querySelector("body");
+    body.style.background = ColorHeandler();
+    e.target.style.background = ColorHeandler();
+    e.target.style.color = ColorHeandler();
+    e.target.style.borderColor = ColorHeandler();
+  };
+
+  const ColorHeandler = () => {
+    let letter = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letter[Math.floor(Math.random() * 16)];
+    }
+    return color;
   };
   return (
-    <div>
+    <div className="container text-center">
       <Title text={"Randomize Color"} />
       <button className="btn btn-primary me-2" onClick={colorHandler}>
         Click Me
@@ -18,7 +32,7 @@ const RandomizeColors = () => {
       <button className="btn btn-success me-2" onClick={colorHandler}>
         Click Me
       </button>
-      <button className="btn btn-primary " onClick={colorHandler}>
+      <button className="btn btn-warning " onClick={colorHandler}>
         Click Me
       </button>
     </div>
--- src/Title.jsx
+++ src/Title.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Title = ({ text }) => {
-  return <div className="title">{!text ? "Title" : text}</div>;
+  return (
+    <div className="title">
+      <h1 className="mb-4 mt-5">{!text ? "Title" : text}</h1>
+    </div>
+  );
 };
 
 export default Title;
